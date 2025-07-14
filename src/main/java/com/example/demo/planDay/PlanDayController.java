9221268594a41947f0defffad7e86a93877d5423
tjddhdl <tjddhdl@gmail.com>
2025-07-14 15:29:20 +0900

    dd

--- src/main/java/com/example/demo/planDay/PlanDayController.java
+++ src/main/java/com/example/demo/planDay/PlanDayController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,10 @@ public class PlanDayController {
 		return ResponseEntity.ok(list);
 	}
 	
-	
+	@PostMapping("/modify")
+	public ResponseEntity<Boolean> planDayModify(@RequestParam PlanDayDto dto, Principal principal){
+		service.modify(dto);
+		return ResponseEntity.ok(true);
+	}
 	
 }
